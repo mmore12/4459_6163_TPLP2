03c6745571af1bba54f1a4371ecbc87b37d79705
elden <elden.carones@gmail.com>
2020-05-19 22:58:20 +0100

    New Solution created

---
+++ TP_LP2_4459_6163/TP_LP2_4459_6163/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_LP2_4459_6163
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
