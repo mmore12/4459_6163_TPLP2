1b97214c8b293987facdd00008734a8568e0d2a2
mmore12 <a6163@alunos.ipca.pt>
2020-05-21 12:16:37 +0100

    excecoes

--- TP_LP2_4459_6163/BR/RegrasParque.cs
+++ TP_LP2_4459_6163/BR/RegrasParque.cs
@@ -9,23 +9,23 @@ using System.Threading.Tasks;
 namespace BR
 {
     /// <summary>
-    /// 
+    /// Regras negocio para gerir o parque
     /// </summary>
     public class RegrasParque
     {
+        #region Regras para gerir caes
+        /// <summary>
+        /// Regra negocio para inserir um cao
+        /// </summary>
+        /// <param name="c">cão a inserir</param>
+        /// <returns>adiciona o cao e retorna verdadeiro, caso contrario retorna falso</returns>
         public static bool InsereCao (Cao c)
         {
-            try
-            {
-                if(c.Id < 0)
-                    return false;
-                else
-                    return DadosParque.AddCao(c);
-            }
-            catch(Exception e)
-            {
-                throw e;
-            }
+            if(c.Id < 0)
+               return false;
+            else
+               return DadosParque.AddCao(c);
         }
+        #endregion
     }
 }
---
+++ TP_LP2_4459_6163/Excecoes/Excecao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excecoes
+{
+    public class Excecao : ApplicationException
+    {
+        public Excecao() : base("Erro")
+        {
+
+        }
+
+        public Excecao(string e) : base(e) 
+        { 
+        }
+
+        public Excecao(string s, Exception e)
+        {
+            throw new Excecao("Erro: " + s + " " + e.Message);
+        }
+    }
+}
